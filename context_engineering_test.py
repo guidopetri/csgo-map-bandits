b1de2954f6eaf93a3dae68f4871ffb864d39d7e7
Alec Hon <alechon2010@gmail.com>
2021-04-20 21:47:54 -0400

    Create basic context engineering

---
+++ context_engineering_test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import context_engineering_functions as cef
+import sys
+
+
+def main(datapath):
+    context = cef.create_basic_triples(datapath, save = False)
+    print(context.head(10))
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+    main(filepath)
+
