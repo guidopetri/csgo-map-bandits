da803a6902ea59122a78b412e705eb061bd2761e
Michael Stanley <michael.hamer.stanley@gmail.com>
2021-05-02 09:21:29 -0400

    Added default policy for team IDs not in training.

--- logging_policy_test.py
+++ logging_policy_test.py
@@ -19,17 +19,36 @@ full_context = map_pick_context[context_cols]
 full_action = map_pick_context['X_Action']
 
 lp = LoggingPolicy(map_pick_context,map_pick_context['X_Action'])
+
+print("\n**********************************\nTest 1: Compare predict_proba to manual calculation for Team ID in the dataset")
 context = full_context.loc[100]
 full_pa_x = lp.pa_x_dict[context['DecisionTeamId']]
-
 # Calculate probability distributions
 from_lp = lp.predict_proba(context)[0]
-manual = full_pa_x[0]/(1-full_pa_x[4]-full_pa_x[4])
+manual = full_pa_x[0]/(1-full_pa_x[3]-full_pa_x[4])
 
 print("predict_proba: ",from_lp)
 print("manual calculation: ",manual)
 
-if lp.predict_proba(context)[0] == manual:
+if from_lp == manual:
+    print("Good")
+else:
+	print("Test Failed")
+
+
+print("\n**********************************\nTest 2: Compare predict_proba to manual calculation for Team ID NOT in the dataset")
+
+# What error comes up when teamid isn't in lp.pa_x_dict?
+context_fake = full_context.loc[100]
+# Use fake Team ID
+context_fake['DecisionTeamId'] = 123456
+full_pa_x = lp.pa_x_dict['default']
+manual = full_pa_x[0]/(1-full_pa_x[3]-full_pa_x[4])
+from_lp = lp.predict_proba(context_fake)[0]
+print("predict_proba: ",from_lp)
+print("manual calculation: ",manual)
+
+if from_lp == manual:
     print("Good")
 else:
 	print("Test Failed")
